2fcfd0239667998cd83bfb5ce0d53aa96e68d711
Lorenzo Casini - lorenzo.casini@studio.unibo.it <STUDENTI\lorenzo.casini@isi-raptor03.campusfc.dir.unibo.it>
2019-09-08 17:12:17 +0200

    adding the test file

--- ProgettoHPC1819/src/test.sh
+++ ProgettoHPC1819/src/test.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+rm graph-image/omp
+rm graph-image/serial
+
 nsteps=100000
 n=256
 
+clear
+
+echo Testing the serial version...
 echo > graph-image/serial
 ./earthquake ${nsteps} ${n} > graph-image/serial
 
 for th in $(seq 1 12);
 do
-  echo > graph-image/omp-${th} 
-  OMP_NUM_THREADS=${th} ./omp-earthquake ${nsteps} ${n} > graph-image/omp-${th}
+  echo Testing the OpenMP versione with ${th} threads... 
+  OMP_NUM_THREADS=${th} ./omp-earthquake ${nsteps} ${n} >> graph-image/omp
 done
+
+echo All test are done.
 
 make clean
