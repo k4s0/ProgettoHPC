56dab52e4217f0b2c6bfcbcbe086a6d3287ae43a
Lorenzo Casini - lorenzo.casini@studio.unibo.it <STUDENTI\lorenzo.casini@isi-raptor03.campusfc.dir.unibo.it>
2019-09-25 06:52:24 +0200

    stable version and correct result test for omp

--- ProgettoHPC1819/src/test.sh
+++ ProgettoHPC1819/src/test.sh
@@ -18,8 +18,9 @@ mv serial_out graph-image/
 for th in $(seq 1 12);
 do
   echo Testing the OpenMP version with ${th} threads... 
-  OMP_NUM_THREADS=${th} ./omp-earthquake ${nsteps} ${n} >> omp_out 
-  mv omp_out graph-image/ 
+  OMP_NUM_THREADS=${th} ./omp-earthquake ${nsteps} ${n} omp_out_${th} 
+  cat omp_out_${th} >> omp_final
+  mv omp_out_${th} graph-image/ 
 done
 
 echo All test are done.
